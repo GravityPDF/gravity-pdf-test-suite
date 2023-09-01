298c73bce8ae3459f158786c4f0a0c70c219c09f
Jake Jackson <jakejackson1@users.noreply.github.com>
2023-09-01 14:46:10 +1000

    Update mpdf-configuration.php
    Only override the required config keys

--- src/mpdf-configuration.php
+++ src/mpdf-configuration.php
@@ -16,7 +16,7 @@
  * @since 0.1
  */
 $config = static function( $config ) {
-	return [
+	return array_merge( $config, [
 		'tempDir'  => sys_get_temp_dir(),
 		'fontDir'  => __DIR__ . '/fonts/',
 		'fontdata' => [
@@ -30,23 +30,7 @@ $config = static function( $config ) {
 		'backupSubsFont' => [],
 		'backupSIPFont'  => '',
 		'BMPonly'        => [ 'dejavusanscondensed' ],
-
-		'curlCaCertificate'  => ABSPATH . WPINC . '/certificates/ca-bundle.crt',
-		'curlFollowLocation' => true,
-
-		'allow_output_buffering' => true,
-		'autoLangToFont'         => false,
-		'useSubstitutions'       => false,
-		'ignore_invalid_utf8'    => true,
-		'setAutoTopMargin'       => 'stretch',
-		'setAutoBottomMargin'    => 'stretch',
-		'enableImports'          => true,
-		'use_kwt'                => true,
-		'keepColumns'            => true,
-		'biDirectional'          => true,
-		'showWatermarkText'      => true,
-		'showWatermarkImage'     => true,
-	];
+	] );
 };
 
 add_filter( 'gfpdf_mpdf_class_config', $config );
